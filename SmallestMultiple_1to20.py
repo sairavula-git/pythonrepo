2138292f86b109f60a15ff8f8dbe249dd2e511a5
hellosai-github <hellosai.devops+git@gmail.com>
2022-09-21 18:52:20 +0530

    made changes for efficiency

--- SmallestMultiple_1to20.py
+++ SmallestMultiple_1to20.py
@@ -1,14 +1,17 @@
 #This is a program to find the smallest integer that is divisible by all numbers from 1 to 20
-#This is the solution to 4th problem in ProjectEuler series
+#This is the solution to 5th problem in ProjectEuler series
 
 answr = 0
-for x in range(100000000, 1000000000):
+x = 20
+check = 0
+while(check != 19):
     check = 0
-    for y in range(2, 21):
-        if x % y == 0:
+    for y in range(20,1,-1):
+        if (x%y) != 0:
+            break
+        else:
             check = check + 1
-    if check == 19:
-        answr = x
-        break
-print(answr)
+    x = x + 20
+    
+print(x-20)
     
